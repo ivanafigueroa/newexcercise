5a38ffce5418af888e6c61743a2aeb2cf5add03e
Iris Ivana Figueroa <figuesco15@gmail.com>
2024-04-30 15:48:34 -0600

    new documents

---
+++ excerscise.js
@@ -0,0 +1,40 @@
+/*
+Crear tarjeta
+Crear Secciones p/tarjeta
+img
+Nombre, desc, edad, lista de algo
+[] Insertar en la tarjeta
+[] Mostrar dentro de div.container
+*/
+
+const CARDS_CONTAINER = document.querySelector("#musica");
+const GENDER = {
+    id:1,
+    genmusic: "Soundtrack",
+    desc: "sound quiet",
+    time: 3,
+    fav_gender: {
+        gender: ["pop", "Banda", "Rock", "Metal"],
+    },
+};
+
+// Crea tarjeta 
+
+const music = document.createElement("div");
+const genmusic_section = document.createElement("h3");
+const desc_section = document.createElement("p");
+const time_section = document.createElement("p");
+const fav_gender = document.createElement("div");
+
+//Lista
+
+const gendermusic = GENDER.fav_gender.gender.map(e =>{
+    const item = document.createElement("ul");
+    item.textContent = e;
+    return item;
+})
+ console.log(music)
+
+ // Crear segunda tarjeta
+
+ genmusic_section = textContent = User. User
---
+++ index.js
@@ -0,0 +1,17 @@
+const div1 = document.getElementById("div-1");
+console.log(div1);
+
+const divCollection = document.getElementsByTagName("div-2");
+console.log(divCollection);
+
+const p = document.getElementById("p1");
+const usernameInputs = document.getElementsByName("username");
+const usernameInput = usernameInputs[0];
+
+console.log(p,usernameInput);
+
+//Events
+
+usernameInput.addEventListener("input", (event) => {console.log(event.target.value)
+p.innerText = event.target.value;});
+
